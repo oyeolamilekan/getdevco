e2dc24508b4291205b34de7336305e18f2d1b6c0
oyejohnson <johnsonoye34@gmail.com>
2018-09-19 11:51:26 +0100

    jfjjfdc

--- website/settings.py
+++ website/settings.py
@@ -123,7 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 if os.getcwd() == '/app':
     import dj_database_url
